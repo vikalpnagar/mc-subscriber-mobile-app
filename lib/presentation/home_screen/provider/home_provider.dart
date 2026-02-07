6c4295d2897afc816360c155c4922ba07ef861d2
Vikalp Nagar <vikalp.nagar@yahoo.com>
2026-02-07 19:01:36 +0530

    Remove Board Id for topology callout

--- lib/presentation/home_screen/provider/home_provider.dart
+++ lib/presentation/home_screen/provider/home_provider.dart
@@ -87,11 +87,6 @@ class HomeProvider with BaseBloc {
 
       if (result.isSuccess) {
         topologyInfo.value = result.message;
-        // topologyInfo.value = TopologyInfo.fromJson(
-        //   jsonDecode(
-        //     '{"boardId":"5591dbe0-f836-4a92-83a2-d5c0b75b10a","edges":{"mesh":[],"wired":[]},"external":[],"nodes":[{"aps":[{"band":"2","bssid":"dc:62:79:65:23:34","channel":6,"clients":[{"connected":3551,"inactive":8,"rssi":-40,"rx_rate_bitrate":6000,"rx_rate_chwidth":20,"station":"fe:d1:47:83:81:61","tx_rate_bitrate":65000}],"mode":"ap","ssid":"TestSSID-2G","timestamp":"2025-11-12T12:28:09+05:30"},{"band":"5","bssid":"dc:62:79:65:23:33","channel":136,"clients":null,"mode":"ap","ssid":"TestSSID-5G","timestamp":"2025-11-12T12:28:09+05:30"}],"mesh":[],"serial":"AA:BB:CC:DD:EE:FF"}],"timestamp":"2025-11-12T06:58:09Z"}',
-        //   ),
-        // );
       } else if (result.sessionExpired) {
         NavigatorService.pushNamedAndRemoveUntil(AppRoutes.loginScreen);
       } else {
